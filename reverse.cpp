1eafd453d80884ac6d0b5993c0cfeeec5513e935
Anmol-1109 <anmolvats11.09@gmail.com>
2025-09-28 20:29:30 +0530

    correction acc to ques

--- reverse.cpp
+++ reverse.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <math.h>
+#include <limits>
 using namespace std;
 int main(){
 cout<<"enter the no ";
@@ -7,16 +8,26 @@ int n;
 int no=0;
 cin>>n;
 int i=0;
-while(n!=0){
-int digit =n%10;
-//no= digit*(pow(10,i))+no;
-no = no*10+ digit;
+/*if(n>=INTMAX_MAX/10||n<INTMAX_MIN){
+    cout<<"enter a valid no";
+    cout<<0;
+    n=0;
+}*/
 
+  while(n!=0){
+int digit =n%10;
+no = no*10+ digit;
 i++;
 n=n/10;
-}
-cout<<"\nno reversed is "<<no;
+if(no>=INT32_MAX/10||no<INT32_MIN/10){
+    break;
+}}
+if(no>=INT32_MAX/10||no<INT32_MIN/10){
+    cout<<0;}
+else{
+cout<<"no reversed is "<<no;}
 
 
+//cout<<'\n'<<INT32_MAX;
 }
 
